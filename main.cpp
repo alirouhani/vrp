0d9424bc2ba01bae5a1eb82c8a1c59d5c2778739
alirouhani <alirouhani.ie@gmail.com>
2026-02-28 04:44:39 -0500

    new step

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "include/function.h"
+
+int main() {
+    print_message("Hello, World!");
+    return 0;
+}
---
+++ src/function.cpp
@@ -0,0 +1,6 @@
+#include "../include/function.h"
+#include <cstdio>
+
+void print_message(const char* message) {
+    printf("%s\n", message);
+}
