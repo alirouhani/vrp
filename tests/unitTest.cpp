b4bc14f08b83d4bb7f0b33e4384080bfafa91e5a
alirouhani <alirouhani.ie@gmail.com>
2026-03-02 02:44:45 -0500

    make error

--- tests/unitTest.cpp
+++ tests/unitTest.cpp
@@ -5,7 +5,7 @@ TEST(FunctionTest, PrintMessage) {
     testing::internal::CaptureStdout();
     print_message("Hello, Test!");
     std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "Hello, Test!\n");
+    EXPECT_EQ(output, "Hello, Test0!\n");
 }
 
 TEST(print_message, EmptyString) {
