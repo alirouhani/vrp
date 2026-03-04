dce7f26b87424d0211db9d2c32e5e700ed7ca145
alirouhani <alirouhani.ie@gmail.com>
2026-03-04 01:39:29 -0500

    null path string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "include/Params.h"
 #include <iostream>
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        return 1;
+    }
     std::string file_path = argv[1];
     Params params;
     params.print_message("Hello, World!");
